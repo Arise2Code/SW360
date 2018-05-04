120f4c25ee408fddeb7d6f18038b67eaf7e25dc5
Scott Weaver <30515614+Arise2Code@users.noreply.github.com>
2018-05-03 20:37:49 -0700

    initial

---
+++ app.js
@@ -0,0 +1,43 @@
+'use strict';
+
+new Vue({
+	el: '#vue-app',
+	data: {
+		mysel: 1,
+		mylinecolor: 'burlywood',
+		mybuttonfont: 'saddlebrown',
+		nonfiction: false,
+		myabout: false,
+		email_submit: false,
+		fname: 'enter first name',
+		email: 'enter email',
+		email_bad: false
+	},
+	methods: {
+		updateView: function(choice){
+			this.mysel = choice;
+			if (choice == 1) {
+				this.mylinecolor = "burlywood";
+				this.mybuttonfont = "saddlebrown";
+			} else if (choice == 2) {
+				this.mylinecolor = "burlywood";
+				this.mybuttonfont = "saddlebrown";
+			} else if (choice == 3) {
+				this.mylinecolor = "IndianRed";
+				this.mybuttonfont = "saddlebrown";
+			} else if  (choice == 4) {
+				this.mylinecolor = "IndianRed";
+				this.mybuttonfont = "saddlebrown";
+			} else {
+				this.mylinecolor = "steelblue";
+				this.mybuttonfont = "DimGray";
+			} 
+		}
+	
+
+	},
+	computed: {
+
+	}
+
+});
